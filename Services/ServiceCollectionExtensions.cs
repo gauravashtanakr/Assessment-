2f25c238e80f647fff2692c4dc8681897faa3dc8
gauravashtankar <88531158+gauravashtankar@users.noreply.github.com>
2021-08-18 20:37:53 +0530

    Add files via upload

---
+++ Services/ServiceCollectionExtensions.cs
@@ -0,0 +1,58 @@
+﻿using DAL.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static object Configuration { get; private set; }
+
+        public static IServiceCollection AddDatabase(this IServiceCollection services)
+        {
+            // Configure DbContext with Scoped lifetime   
+            services.AddDbContext<InterviewContext>(options =>
+            {
+                options.UseSqlServer(ConnectionString.InterviewDatabase,
+                    sqlOptions => sqlOptions.CommandTimeout(120)).UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            }, ServiceLifetime.Transient
+            );
+            
+            return services;
+        }
+
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<LossTypeService>();
+            services.AddScoped<UserService>();
+            return services;
+        }
+
+        public static IServiceCollection AddInterviewAuthentication(this IServiceCollection services)
+        {
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(90);
+                options.LoginPath = "Account/Login";
+                options.AccessDeniedPath = "/404";
+                options.SlidingExpiration = true;
+
+            });
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
+            {
+                options.LoginPath = "/Account/Login";
+            });
+            return services;
+        }
+    }
+}
---
+++ Services/UserService.cs
@@ -0,0 +1,27 @@
+﻿using DAL.Models;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class UserService
+    {
+        private readonly InterviewContext _interviewContext;
+        private readonly ILogger<UserService> _logger;
+
+        public UserService(InterviewContext interviewContext, ILogger<UserService> logger)
+        {
+            _interviewContext = interviewContext ?? throw new ArgumentNullException(nameof(interviewContext));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task<User> IsUserAuthenticate(User user)
+        {
+            return await Task.Run(() => _interviewContext.Users.Where(a => a.UserName == user.UserName && a.Password == user.Password && a.Active == true).FirstOrDefault());
+        }
+    }
+}
