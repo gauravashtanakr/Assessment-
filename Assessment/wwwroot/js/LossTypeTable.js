6bf087415ec3c0e7fb8eefb0902473a550c5c5cd
gauravashtankar <88531158+gauravashtankar@users.noreply.github.com>
2021-08-18 20:26:02 +0530

    First Commit -- Part 3

---
+++ Assessment/wwwroot/js/LossTypeTable.js
@@ -0,0 +1,36 @@
+﻿$(document).ready(function () {
+
+$('#table').on('processing.dt', function (e, settings, processing) {
+    $('#table_processing').css('display', processing ? 'block' : 'none');
+}).DataTable({
+    processing: true,
+    serverSide: true,
+    searchable: true,
+    scrollY: '50vh',
+    pageLength: 100,
+    ajax: {
+        url: '../api/LossType',
+        type: 'POST',
+        dataSrc: 'data',
+    },
+    oLanguage: {
+        sProcessing: "<div class='text-center' >\n\
+                    <div class= 'spinner-border' role = 'status' >\n\
+                    </div><div>Processing ...</div></div>"
+    },
+    "order": [[0, "desc"]],
+    "columnDefs": [
+        {
+            'data': 'lossTypeId', 'name': 'LossTypeId', type: 'int', 'targets': [0]
+        },
+        {
+            'data': 'lossTypeCode', 'name': 'LossTypeCode', type: 'int', 'targets': [1]
+        },
+        { 'data': 'lossTypeDescription', 'name': 'LossTypeDescription', 'targets': [2] }
+    ],
+    "scrollX": true,
+    "scrollY": 357,
+});
+
+   
+});
